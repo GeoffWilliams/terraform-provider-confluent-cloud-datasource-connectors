0ea3ad52c99ec032ffc7eb6589082dda653eecdd
Geoff Williams <gwilliams@confluent.io>
2023-03-05 12:24:37 +1100

    example

--- testcase/test.tf
+++ testcase/test.tf
@@ -8,23 +8,32 @@ terraform {
   }
 }
 
-locals {
-  connectors_to_lookup = {"aconnector":{}, "anotherconnector":{}}
+provider "confluent-cloud-datasource-connectors" {
+#  cloud_api_key    = var.confluent_cloud_api_key    # optionally use CONFLUENT_CLOUD_API_KEY env var
+#  cloud_api_secret = var.confluent_cloud_api_secret # optionally use CONFLUENT_CLOUD_API_SECRET env var
 }
 
-data "confluent-cloud-datasource-connectors" "connectors" {
+locals {
+  connectors_to_lookup = {"gwilliams-cpsconnect-discountCodes":{}, "gwilliams-mssql-jdbc":{}, "nonexistantconnector": {}}
+}
+
+data "confluent-cloud-datasource-connectors" "confluent_connectors" {
   for_each = local.connectors_to_lookup
-  environment_id = "anenv"
-  kafka_cluster_id = "deadbeef"
+  environment_id = "env-w5502w"
+  kafka_cluster_id = "lkc-7n1ny1"
   connector_name = each.key
 }
 
-output "aconnector" {
-  value = data.confluent-cloud-datasource-connectors.connectors["aconnector"]
+output "gwilliams-cpsconnect-discountCodes" {
+  value = data.confluent-cloud-datasource-connectors.confluent_connectors["gwilliams-cpsconnect-discountCodes"]
 }
 
-output "anotherconnector" {
-  value = data.confluent-cloud-datasource-connectors.connectors["anotherconnector"]
+output "gwilliams-mssql-jdbc" {
+  value = data.confluent-cloud-datasource-connectors.confluent_connectors["gwilliams-mssql-jdbc"]
+}
+
+output "nonexistantconnector" {
+  value = data.confluent-cloud-datasource-connectors.confluent_connectors["nonexistantconnector"]
 }
 
 output "bump" {
